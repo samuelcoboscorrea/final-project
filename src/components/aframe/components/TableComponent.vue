31060daef11cbc1d4d057e643baac98d67e0db1f
s.cobosc <s.cobosc@alumnos.urjc.es>
2024-03-20 11:22:34 +0100

    update last changes

--- src/components/aframe/components/TableComponent.vue
+++ src/components/aframe/components/TableComponent.vue
@@ -1,26 +1,35 @@
 <template>
 <a-entity position="0 0.5 -5">
-        <!-- Mesa -->
-        <a-entity position="0 0 0" >
-          <!-- Superficie de la mesa -->
-          <a-box width="2" height="0.1" depth="1" color="brown" rotation="10 0 0"></a-box>
-          <!-- Patas de la mesa -->
-          <a-entity position="-0.9 -0.55 -0.4">
-            <a-box width="0.1" height="1" depth="0.1" color="gray"></a-box>
-          </a-entity>
-          <a-entity position="0.9 -0.55 -0.4">
-            <a-box width="0.1" height="1" depth="0.1" color="gray"></a-box>
-          </a-entity>
-          <a-entity position="-0.9 -0.55 0.4">
-            <a-box width="0.1" height="1" depth="0.1" color="gray"></a-box>
-          </a-entity>
-          <a-entity position="0.9 -0.55 0.4">
-            <a-box width="0.1" height="1" depth="0.1" color="gray"></a-box>
-          </a-entity>
-        </a-entity>
-        
-        <!-- Cuadro de mandos -->
-        <a-box width="1.5" height="0.1" depth="0.5" position="0 0.15 0" color="gray" rotation="10 0 0"></a-box>
-        <!-- Otros objetos en el cuadro de mandos, como botones, pantallas, etc. -->
-      </a-entity>
+  <!-- Mesa -->
+  <a-entity hello-world position="0 0 0" >
+    <!-- Superficie de la mesa -->
+    <a-box width="2" height="0.1" depth="1" color="brown" rotation="10 0 0"></a-box>
+    <!-- Patas de la mesa -->
+    <a-entity position="-0.9 -0.55 -0.4">
+      <a-box width="0.1" height="1" depth="0.1" color="gray"></a-box>
+    </a-entity>
+    <a-entity position="0.9 -0.55 -0.4">
+      <a-box width="0.1" height="1" depth="0.1" color="gray"></a-box>
+    </a-entity>
+    <a-entity position="-0.9 -0.55 0.4">
+      <a-box width="0.1" height="1" depth="0.1" color="gray"></a-box>
+    </a-entity>
+    <a-entity position="0.9 -0.55 0.4">
+      <a-box width="0.1" height="1" depth="0.1" color="gray"></a-box>
+    </a-entity>
+  </a-entity>
+
+  <a-box width="1.5" height="0.1" depth="0.5" position="0 0.15 0" color="gray" rotation="10 0 0"></a-box>
+</a-entity>
 </template>
+
+<script setup>
+import defineProps from 'vue'
+
+const props = defineProps({
+  testProperty: String
+})
+
+console.log(props.testProperty)
+
+</script>
--- src/components/aframe/scenarios/EditorScenario.vue
+++ src/components/aframe/scenarios/EditorScenario.vue
@@ -6,16 +6,28 @@
           <a-camera></a-camera>
         </a-entity>
 
+        <SelectorComponent/>
+
         <a-grid width="100" height="100" color="#888" rotation="-90 0 0" opacity="0.5"></a-grid>
 
-        <TableComponent/>
+        <a-entity position="0 1.6 0">
+          <a-box width="1.5" height="0.1" depth="0.5" color="gray" rotation="90 0 0" material="color: #779fca; shader: flat; height: 256.22" geometry="height: 0.03; depth: 1.09" visible=""></a-box><a-box width="1.5" height="0.1" depth="0.5" color="gray" rotation="90 0 0" material="color: #ca77c3; shader: flat; height: 256.22" geometry="width: 0.24; height: 0.07; depth: 0.13" visible="" position="-0.01 0 0.04">
+          </a-box>
+        </a-entity>
+
+        <TableComponent :testProperty="'testProperty executed'"/>
       </a-scene>
     </div>
-    
   </div>
 </template>
 
 <script setup>
+// import { ref } from 'vue'
 import TableComponent from '@/components/aframe/components/TableComponent.vue'
-console.log('ini')
+import SelectorComponent from '@/components/aframe/components/SelectorComponent.vue'
+
+// const elements = {
+//   box: null
+// }
+
 </script>

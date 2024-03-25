c1cf921178edf1731196c74cdcc1dc41ed4f2913
System Administrator <root@MacBook-Air-de-Samuel.local>
2024-03-25 17:29:26 +0100

    deploy

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:<USERNAME>/<REPO>.git develop:gh-pages
+
+cd -
